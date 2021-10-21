57374b7efb5f78e9db3e94cad52acf9b5bb2e340
= <fabian.tischler.ft@gmail.com>
2021-10-21 14:33:11 +0200

    TF, 21.10.21, added Person Repo with save Method

--- corona-fit/src/main/java/at/htl/boundary/PersonRepository.java
+++ corona-fit/src/main/java/at/htl/boundary/PersonRepository.java
@@ -1,4 +1,15 @@
 package at.htl.boundary;
 
-public class PersonRepository {
+import at.htl.entities.Person;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
+
+@ApplicationScoped
+public class PersonRepository implements PanacheRepository<Person> {
+    @Transactional
+    public Person save(Person person){
+        return getEntityManager().merge(person);
+    }
 }
